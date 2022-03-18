9407bbab4a9df0393e6192a36e6f5b4dfe48c6bc
Carlos Cerqueira <ce.cesc01@gmail.com>
2022-03-17 23:37:07 -0300

    Add cactus generator

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const dino = document.getElementsByClassName('dino')[0];
+const dino = document.getElementById('dino');
+const background = document.getElementById('bg');
 let dinoJump = false;
 
 document.addEventListener('keyup', event => {
@@ -7,16 +8,17 @@ document.addEventListener('keyup', event => {
   if (key === 'Space' && !dinoJump) {
     dispatchEvent = true;
     let dinoPosition = parseInt(dino.style.bottom) || 5;
+
     const jumpAction = setInterval(() => {
       if (dinoPosition >= 150 ) {
         clearInterval(jumpAction);
 
         const down = setInterval(() => {
-          if (dinoPosition <= 5) {
+          if (dinoPosition <= 20) {
             dinoJump = false;
             clearInterval(down);
           } else {
-            dinoPosition -= 15;
+            dinoPosition -= 5;
             dino.style.bottom = `${dinoPosition}px`;
           }
         }, 10);
@@ -24,8 +26,27 @@ document.addEventListener('keyup', event => {
         dinoPosition += 10;
         dino.style.bottom = `${dinoPosition}px`;
       }
-      
     }, 10);
   }
 });
 
+(function generateCactus() {
+  const cactus = document.createElement('div');
+  cactus.classList.add('cactus');
+  cactus.style.right = `${Math.floor((10))}px`;
+  background.appendChild(cactus);
+
+  const moveCactus = setInterval(() => {
+    let cactusPosition = parseInt(cactus.style.right);
+
+    if (cactusPosition > window.innerWidth) {
+      clearInterval(moveCactus);
+      background.removeChild(cactus);
+    } else {
+      cactusPosition += 10;
+      cactus.style.right = `${cactusPosition}px`;
+    }
+  }, 30);
+
+  setTimeout(generateCactus, Math.floor(Math.random() * 10000) + 1000);
+})();
